e81dafa737d79e08dd5f6fb2f28cff91ed9e9f40
hexun <1226807142@qq.com>
2017-12-29 18:16:29 +0800

    修改存在的问题

--- map_reduce_filter.py
+++ map_reduce_filter.py
@@ -1,31 +1,35 @@
 # -*- coding:utf-8 -*-
+from collections import Iterable
+import functools
+
 def fn(x):
     return x * x
-
 
 def map1(func, iterable):
     if not callable(func):
         raise Exception('func is not callable')
-    if not isinstance(iterable, (list, tuple)):
+    if not isinstance(iterable, (Iterable)):
         raise Exception('Iterable is not Iterabel')
     else:
         return (func(value) for index, value in enumerate(iterable))
+
 
 ret = list(map1(fn, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
 print(ret)
 
 print('------------------')
 
-
 def fn(x, y):
     return x + y
-
 
 def reduce1(func, iterable, initval=0):
     if not callable(func):
         raise Exception('func is not callable')
-    if not isinstance(iterable, (list,tuple)):
+    if not isinstance(iterable, (Iterable)):
         raise Exception('Iterable is not Iterabel')
+
+    if isinstance(iterable,(str)):
+        return iterable
     else:
         l = iterable
         while True:
@@ -33,28 +37,41 @@ def reduce1(func, iterable, initval=0):
                 l[0] = func(l[0], l[1])
                 l.pop(1)
             else:
-                return func(l[0],initval)
+                return func(l[0], initval)
 
-ret = reduce1(fn, [1, 3, 5, 7, 9],100)
+ret = reduce1(fn, [1, 3, 5, 7, 9], 100)
+ret1 = functools.reduce(fn, [1, 3, 5, 7, 9], 100)
+ret2 = reduce1(fn, '1234')
+ret3 = functools.reduce(fn,'1234')
+
 print(ret)
+print(ret1)
+print(ret2)
+print(ret3)
+
+print('------------------')
+
+
+def fn(n):
+    return n % 2 == 1
 
 print('------------------')
 
 def fn(n):
     return n % 2 == 1
 
-def filter1(func,iterable):
+def filter1(func, iterable):
     if not callable(func):
         raise Exception('func is not callable')
-    if not isinstance(iterable, (list, tuple)):
+    if not isinstance(iterable, (Iterable)):
         raise Exception('Iterable is not Iterabel')
     else:
-        l = []
+        cplst = []
         for x in iterable:
             if fn(x):
-                l.append(x)
+                cplst.append(x)
         else:
-            return l
+            return cplst
 
-ret = list(filter1(fn,[1,2,4,5,6,9,10,15]))
+ret = list(filter1(fn, [1, 2, 4, 5, 6, 9, 10, 15]))
 print(ret)
